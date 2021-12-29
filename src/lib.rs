26ee333bd245f24a9c5e7e428ca3c583a430d884
Ginkoe <ginkoe@protonmail.com>
2021-12-29 14:57:13 +0100

    feat(codegen): write to output target (wip)

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,11 @@ extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
 
-use std::io;
+use std::{
+    fs::OpenOptions,
+    io::{self, Write},
+    path::PathBuf,
+};
 
 use config::ConfigManager;
 use log::{debug, info};
@@ -32,14 +36,37 @@ impl Sahih {
         for (project_name, project_config) in self.config.projects {
             info!("Starting generation of project {}", project_name);
             debug!("W/ config :\n{:#?}", project_config);
+
+            debug!("{}", project_config.output.target);
+            std::fs::create_dir_all(&project_config.output.target).unwrap();
+
+            let mut model_file_path = PathBuf::from(&project_config.output.target);
+            model_file_path.push("models.ts");
+
+            let mut output_file = OpenOptions::new()
+                .create(true)
+                .write(true)
+                .truncate(true)
+                .open(model_file_path)
+                .unwrap();
+
+            // TODO: Header file
+            output_file.write_all(
+                br#"// Generated with Sahih
+import yup from "yup";
+
+"#,
+            )?;
             let schemas = consume_schemas(&project_config.input.target);
 
             for model in &schemas {
                 let generator = InterfaceGenerator::from(model);
                 let serialized = generator.build();
                 debug!("Serialized:\n {}", serialized);
+                output_file.write_all(format!("{}\n", serialized).as_bytes())?;
 
                 let generator = ValidationGenerator::from(model);
+                output_file.write_all(format!("{}\n\n\n", generator.build()).as_bytes())?;
                 info!("Serialized:\n {:#?}", generator);
             }
         }
